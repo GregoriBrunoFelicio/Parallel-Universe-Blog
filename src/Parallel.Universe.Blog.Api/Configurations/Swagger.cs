d1d5b1cd55f4303ae43709e208673fe750b56fd7
gregori bruno felicio <gregory.wow@hotmail.com>
2021-04-09 20:44:59 -0300

    rename project

---
+++ src/Parallel.Universe.Blog.Api/Configurations/Swagger.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace Parallel.Universe.Blog.Api.Configurations
+{
+    public static class Swagger
+    {
+        public static void Configure(IServiceCollection services) => services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo { Title = "Parallel.Universe.Blog.Api", Version = "v1" }));
+    }
+}
