be76255d0946104fdd9251e461796ad9eb3ce0a7
gregori bruno felicio <gregory.wow@hotmail.com>
2021-05-02 13:36:43 -0300

    create test to get post by id

--- Tests/Parallel.Universe.Blog.Tests/Integration Tests/PostTests.cs
+++ Tests/Parallel.Universe.Blog.Tests/Integration Tests/PostTests.cs
@@ -1,8 +1,10 @@
 ﻿using FluentAssertions;
 using NUnit.Framework;
 using Parallel.Universe.Blog.Api.Data.Repositories;
+using Parallel.Universe.Blog.Api.Entities;
 using Parallel.Universe.Blog.Tests.Shared;
 using Parallel.Universe.Blog.Tests.Shared.Builders;
+using System;
 using System.Net;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
@@ -12,14 +14,19 @@ namespace Parallel.Universe.Blog.Tests.Integration_Tests
     public class PostTests : IntegrationBase
     {
         protected PostViewModelBuilder postViewModelBuilder;
+        protected PostBuilder postBuilder;
         protected UserRepository userRepository;
+        protected PostRepository postRepository;
+
 
         [OneTimeSetUp]
         public new void SetUp()
         {
 
             postViewModelBuilder = new PostViewModelBuilder();
+            postBuilder = new PostBuilder();
             userRepository = new UserRepository(Context);
+            postRepository = new PostRepository(Context);
         }
     }
 
@@ -35,6 +42,24 @@ namespace Parallel.Universe.Blog.Tests.Integration_Tests
             var model = postViewModelBuilder.Generate();
             model.UserId = user.Id;
             var response = await Client.PostAsJsonAsync("Post", model);
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+    }
+
+    public class PostGetById : PostTests
+    {
+        [Test]
+        public async Task ShouldReturnOk()
+        {
+            var user = new UserBuilder().WithActive(true).Generate();
+            await userRepository.AddAsync(user);
+
+            var post = new Post(0, "Title", "Description", "Text", DateTime.Now, true, user.Id);
+            await postRepository.AddAsync(post);
+
+            await Context.SaveChangesAsync();
+
+            var response = await Client.GetAsync($"Post/{post.Id}");
             response.StatusCode.Should().Be(HttpStatusCode.OK);
         }
     }
