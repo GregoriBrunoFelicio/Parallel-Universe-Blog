d5aa75b72e464cb09268b43e070db05a16a0ea77
gregori bruno felicio <gregory.wow@hotmail.com>
2021-05-16 22:09:37 -0300

    create comment

---
+++ src/Parallel.Universe.Blog.Api/Entities/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Parallel.Universe.Blog.Api.Entities
+{
+    public class Comment : Entity
+    {
+        public Comment(int id, string content, DateTime date) : base(id)
+        {
+            Content = content;
+            Date = date;
+        }
+
+        public string Content { get; }
+        public DateTime Date { get; }
+    }
+}
