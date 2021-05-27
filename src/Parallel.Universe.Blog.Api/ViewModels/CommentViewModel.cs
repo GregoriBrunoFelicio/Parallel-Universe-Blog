82d2ad4053053c25f19e53dba6d56463da71d3de
gregori bruno felicio <gregory.wow@hotmail.com>
2021-05-27 19:47:20 -0300

    add validation to comment content

--- src/Parallel.Universe.Blog.Api/ViewModels/CommentViewModel.cs
+++ src/Parallel.Universe.Blog.Api/ViewModels/CommentViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Parallel.Universe.Blog.Api.ViewModels
 {
     public class CommentViewModel
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "The content is required.")]
         public string Content { get; set; }
         public DateTime Date { get; set; }
         public int PostId { get; set; }
