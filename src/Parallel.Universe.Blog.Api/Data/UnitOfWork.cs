d3bc49db04ad7750f2f34e6a99079292d8af6672
gregori bruno felicio <gregory.wow@hotmail.com>
2021-07-11 15:21:43 -0300

    create unit of work methods

--- src/Parallel.Universe.Blog.Api/Data/UnitOfWork.cs
+++ src/Parallel.Universe.Blog.Api/Data/UnitOfWork.cs
@@ -1,6 +1,21 @@
-﻿namespace Parallel.Universe.Blog.Api.Data
+﻿using System.Threading.Tasks;
+
+namespace Parallel.Universe.Blog.Api.Data
 {
-    public class UnitOfWors
+    public interface IUnitOfWork
     {
+        Task<bool> CommitAsync();
+        Task RoolBack();
+    }
+
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ParallelUniverseBlogContext context;
+
+        public UnitOfWork(ParallelUniverseBlogContext context) => this.context = context;
+
+        public async Task<bool> CommitAsync() => await context.SaveChangesAsync() > 0;
+
+        public async Task RoolBack() => await context.DisposeAsync();
     }
 }
