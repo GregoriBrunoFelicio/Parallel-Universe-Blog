912a16966bc6f8c21aaf124fc962c77b1b2b0fcb
gregori bruno felicio <gregory.wow@hotmail.com>
2021-08-08 22:59:55 -0300

    fix rollback

--- src/Parallel.Universe.Blog.Api/Data/UnitOfWork.cs
+++ src/Parallel.Universe.Blog.Api/Data/UnitOfWork.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using System.Threading.Tasks;
 
 namespace Parallel.Universe.Blog.Api.Data
 {
@@ -11,11 +12,16 @@ namespace Parallel.Universe.Blog.Api.Data
     public class UnitOfWork : IUnitOfWork
     {
         private readonly ParallelUniverseBlogContext context;
+        private readonly IDbContextTransaction transaction;
 
-        public UnitOfWork(ParallelUniverseBlogContext context) => this.context = context;
+        public UnitOfWork(ParallelUniverseBlogContext context)
+        {
+            this.context = context;
+            this.transaction = context.Database.BeginTransaction();
+        }
 
         public async Task<bool> CommitAsync() => await context.SaveChangesAsync() > 0;
 
-        public async Task RollBackAsync() => await context.DisposeAsync();
+        public async Task RollBackAsync() => await transaction.RollbackAsync();
     }
 }
